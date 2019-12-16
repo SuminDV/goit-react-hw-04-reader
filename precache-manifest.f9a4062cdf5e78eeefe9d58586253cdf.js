663165489f8c00fa61c88cb9c67e08bcdd7e5855
SuminDV <sumin.dmytro@gmail.com>
2019-12-16 12:28:09 +0200

    Updates

---
+++ precache-manifest.f9a4062cdf5e78eeefe9d58586253cdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cd774871e6b62f279779c67575bb85a",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "ec9d37d58379f61e7082",
+    "url": "/goit-react-hw-04-reader/static/css/reader-page.9894b96b.chunk.css"
+  },
+  {
+    "revision": "b77d94bf66451927335b",
+    "url": "/goit-react-hw-04-reader/static/js/3.5267989d.chunk.js"
+  },
+  {
+    "revision": "105075e91919e621f909",
+    "url": "/goit-react-hw-04-reader/static/js/main.113018d5.chunk.js"
+  },
+  {
+    "revision": "ec9d37d58379f61e7082",
+    "url": "/goit-react-hw-04-reader/static/js/reader-page.fbd4cd93.chunk.js"
+  },
+  {
+    "revision": "3c6b0a54a0626807f7de",
+    "url": "/goit-react-hw-04-reader/static/js/runtime-main.9772f26a.js"
+  }
+]);
